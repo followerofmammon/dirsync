7a3bf8410a78c305e86fb1282363920733765beb
Eliran Levi <eliran@stratoscale.com>
2017-09-04 18:22:27 +0300

    Change main menu to picking from where to sync

--- dirsync/main.py
+++ dirsync/main.py
@@ -1,7 +1,7 @@
 import os
 
 import printer
-import treeprinter
+import optionpicker
 import replicas_from_args
 import interactive_dir_sync
 import search_supplement_replicas
@@ -13,9 +13,21 @@ def sync_from_src_to_dst(src, dst):
 
 
 def sync(replica_a, replica_b):
-    sync_from_src_to_dst(src=replica_a, dst=replica_b)
-    sync_from_src_to_dst(src=replica_b, dst=replica_a)
+    A_TO_B = "Remove/Copy files from %s to %s" % (replica_a, replica_b)
+    B_TO_A = "Remove/Copy files from %s to %s" % (replica_b, replica_a)
+    QUIT = "Quit"
+    options = [A_TO_B, B_TO_A, QUIT]
 
+    option = None
+    picker = optionpicker.OptionPickerByMenuTraverse(options)
+    while option != QUIT:
+        option = picker.pick_one()
+        if option == A_TO_B:
+            sync_from_src_to_dst(src=replica_a, dst=replica_b)
+        elif option == B_TO_A:
+            sync_from_src_to_dst(src=replica_b, dst=replica_a)
+        elif option is None:
+            break
 
 def main():
     replica_a_path, replica_b_path = replicas_from_args.get()
