b4977db5192e8d3fc83c003c6e958c89a7e4d075
Eliran Levi <eliran@stratoscale.com>
2017-09-09 16:27:45 +0300

    Fix pep8 errors in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    setup_requires = [
+    setup_requires=[
         'pbr >= 1.9',
         'setuptools >= 17.1'
     ],
-    pbr = True
+    pbr=True
 )
