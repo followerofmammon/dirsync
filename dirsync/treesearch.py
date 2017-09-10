89ca5565cf7463731d3485f2e8566ac8749b0879
Eliran Levi <eliran@stratoscale.com>
2017-09-10 19:44:45 +0300

    Optimize tree searching a bit more

--- dirsync/treesearch.py
+++ dirsync/treesearch.py
@@ -1,4 +1,3 @@
-import re
 import collections
 
 import treelib
@@ -8,60 +7,59 @@ class TreeSearch(object):
     TREE_CACHE_SIZE = 20
 
     def __init__(self, tree):
-        self._original_tree = tree
-        self._tree = None
-        self._previous_pattern = None
-        self._nodes_that_match_search_filter = dict()
-        self._pattern_to_base_tree_cache = collections.OrderedDict()
+        self._tree = treelib.Tree(tree, deep=True)
+        self._result_cache = collections.OrderedDict()
 
     def get_filtered_tree(self, pattern):
-        self._tree = self._get_base_tree(pattern)
-        if pattern:
-            self._scan_nodes_that_match_search_pattern(pattern)
-            self._filter_matching_nodes()
-        self._previous_pattern = pattern
-        self._populate_tree_cache(pattern)
-        return self._tree
+        if pattern in self._result_cache:
+            tree = self._result_cache.pop(pattern)
+        elif pattern:
+            matching_nodes = self._scan_nodes_that_match_search_pattern(pattern)
+            tree = self._filter_matching_nodes(matching_nodes)
+        else:
+            tree = self._tree
+        self._populate_tree_cache(pattern, tree)
+        return tree
 
-    def _get_base_tree(self, pattern):
-        if pattern in self._pattern_to_base_tree_cache:
-            return self._pattern_to_base_tree_cache.pop(pattern)
-        if self._previous_pattern is None or not pattern.startswith(self._previous_pattern):
-            self._tree = treelib.Tree(self._original_tree, deep=True)
-        return treelib.Tree(self._tree, deep=True)
-
-    def _populate_tree_cache(self, pattern):
-        if len(self._pattern_to_base_tree_cache) > self.TREE_CACHE_SIZE:
-            oldest_keyin_cahe = self._pattern_to_base_tree_cache.keys()[0]
+    def _populate_tree_cache(self, pattern, tree):
+        if len(self._result_cache) > self.TREE_CACHE_SIZE:
+            oldest_keyin_cahe = self._result_cache.keys()[0]
             del oldest_keyin_cahe[oldest_keyin_cahe]
-        self._pattern_to_base_tree_cache[pattern] = self._tree
+        self._result_cache[pattern] = tree
 
     def _scan_nodes_that_match_search_pattern(self, pattern):
-        self._nodes_that_match_search_filter = {self._tree.root: True}
+        matching_nodes = {self._tree.root: True}
         for nid, node in self._tree.nodes.iteritems():
             node_data = node.tag if node.data is None else str(node.data)
-            if re.findall(pattern, node_data):
+            if pattern in node_data:
                 for ancestor_nid in self._tree.rsearch(nid):
-                    self._nodes_that_match_search_filter[ancestor_nid] = True
+                    if ancestor_nid not in matching_nodes:
+                        matching_nodes[ancestor_nid] = True
+                        yield ancestor_nid
 
-    def _filter_matching_nodes(self):
-        while True:
-            for nid, node in self._tree.nodes.iteritems():
-                if nid not in self._nodes_that_match_search_filter:
-                    self._tree.remove_node(node.identifier)
+    def _filter_matching_nodes(self, matching_nodes):
+        tree = treelib.Tree()
+        root = self._tree.get_node(self._tree.root)
+        tree.create_node(tag=root.tag, identifier=root.identifier, data=root.data)
+        for matching_nid in matching_nodes:
+            nodes_to_add_gen = self._tree.rsearch(matching_nid)
+            nodes_to_add = list()
+            for nid in nodes_to_add_gen:
+                if nid in tree.nodes:
                     break
-            else:
-                break
+                else:
+                    nodes_to_add.insert(0, nid)
+            for nid in nodes_to_add:
+                node = self._tree.get_node(nid)
+                tree.create_node(tag=node.tag,
+                                 identifier=node.identifier,
+                                 data=node.data,
+                                 parent=node.bpointer)
+        return tree
 
-    def _interactive_search_iteration(self):
-        result = self._scan_char()
-        if result == self._SEARCH_RESULT_SERACH_ENDED:
-            if not self._search_pattern:
-                self._tree = treelib.Tree(self._original_tree, deep=True)
-        elif result == self._SERACH_RESULT_SERACH_CONTINUES:
-            self._tree = treelib.Tree(self._original_tree, deep=True)
-            self._scan_nodes_that_match_search_pattern()
-            self._filter_matching_nodes()
-            result = self._SERACH_RESULT_SERACH_CONTINUES
-        else:
-            assert False, result
+if __name__ == "__main__":
+    import dirtree
+    _dir = dirtree.DirTree.factory_from_filesystem('/home/eliran/Music')
+    search = TreeSearch(_dir)
+    import cProfile
+    cProfile.run("search.get_filtered_tree('B')")
