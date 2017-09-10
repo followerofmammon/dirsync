a18614b5050942a263fb63c8668d24207b1ff297
Eliran Levi <eliran@stratoscale.com>
2017-09-10 15:30:05 +0300

    Add a tree cache for treesearch for performance optimization

--- dirsync/treesearch.py
+++ dirsync/treesearch.py
@@ -1,22 +1,40 @@
 import re
+import collections
+
 import treelib
 
 
 class TreeSearch(object):
+    TREE_CACHE_SIZE = 20
+
     def __init__(self, tree):
         self._original_tree = tree
         self._tree = None
         self._previous_pattern = None
         self._nodes_that_match_search_filter = dict()
+        self._pattern_to_base_tree_cache = collections.OrderedDict()
 
     def get_filtered_tree(self, pattern):
-        if self._previous_pattern is None or not pattern.startswith(self._previous_pattern):
-            self._tree = treelib.Tree(self._original_tree, deep=True)
+        self._tree = self._get_base_tree(pattern)
         if pattern:
             self._scan_nodes_that_match_search_pattern(pattern)
             self._filter_matching_nodes()
         self._previous_pattern = pattern
+        self._populate_tree_cache(pattern)
         return self._tree
+
+    def _get_base_tree(self, pattern):
+        if pattern in self._pattern_to_base_tree_cache:
+            return self._pattern_to_base_tree_cache.pop(pattern)
+        if self._previous_pattern is None or not pattern.startswith(self._previous_pattern):
+            self._tree = treelib.Tree(self._original_tree, deep=True)
+        return treelib.Tree(self._tree, deep=True)
+
+    def _populate_tree_cache(self, pattern):
+        if len(self._pattern_to_base_tree_cache) > self.TREE_CACHE_SIZE:
+            oldest_keyin_cahe = self._pattern_to_base_tree_cache.keys()[0]
+            del oldest_keyin_cahe[oldest_keyin_cahe]
+        self._pattern_to_base_tree_cache[pattern] = self._tree
 
     def _scan_nodes_that_match_search_pattern(self, pattern):
         self._nodes_that_match_search_filter = {self._tree.root: True}
