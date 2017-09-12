e815a370c49478c09d29a3d27004c654924ce45e
Eliran Levi <eliran@stratoscale.com>
2017-09-12 21:50:00 +0300

    Ignore file_entry in fspicker if None

--- dirsync/filesystem_chooser.py
+++ dirsync/filesystem_chooser.py
@@ -31,16 +31,18 @@ def pick_wrapper(picker):
 def main():
     args = parse_args()
     picker = FilesystemPicker(args.dir)
-    if os.getenv('MODE') == 'static':
-        file_entry = picker.pick_one()
+    global picked_file
+    if os.getenv('MODE') == 'nonstatic':
+        picked_file = picker.pick_one()
     else:
         printer.wrapper(pick_wrapper, picker)
-        file_entry = picked_file
-    if args.execute is not None:
-        command = [os.path.basename(args.execute), file_entry.full_filesystem_path()]
-        os.execve(args.execute, command, {})
-    else:
-        printer.print_string(file_entry.full_filesystem_path())
+    if picked_file is not None:
+        if args.execute is not None:
+            command = [os.path.basename(args.execute), picked_file.full_filesystem_path()]
+            os.execve(args.execute, command, {})
+        else:
+            printer.print_string(picked_file.full_filesystem_path())
+
 
 if __name__ == "__main__":
     main()
