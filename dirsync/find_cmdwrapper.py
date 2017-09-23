bd71a8e1886722eec0dde84d653e8a309eb3f55d
Eliran Levi <eliran@stratoscale.com>
2017-09-23 22:56:12 +0300

    Fix bad usage of 'find' command

--- dirsync/find_cmdwrapper.py
+++ dirsync/find_cmdwrapper.py
@@ -3,21 +3,21 @@ import subprocess
 
 def find_files(rootpath, max_depth=None, file_extentions=None):
     command = _get_base_command(rootpath)
-    command += ["-type", "f"]
+    if max_depth is not None:
+        command.extend(["-maxdepth", str(max_depth)])
     if file_extentions is not None:
         command.append("-regex")
         addition = ".*." + "\|.*.".join(file_extentions)
         command.append(addition)
-    if max_depth is not None:
-        command.extend(["-maxdepth", str(max_depth)])
+    command += ["-type", "f"]
     return _get_command_output(command)
 
 
 def find_dirs(rootpath, max_depth):
     command = _get_base_command(rootpath)
-    command += ["-type", "d"]
     if max_depth is not None:
         command.extend(["-maxdepth", str(max_depth)])
+    command += ["-type", "d"]
     return _get_command_output(command)
 
 
@@ -25,5 +25,6 @@ def _get_base_command(rootpath):
     return ["find", rootpath]
 
 def _get_command_output(command):
+    print ' '.join(command)
     output = subprocess.check_output(command)
     return output.splitlines()
