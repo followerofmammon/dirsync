be6cc80976d1b22f1cb0e19af4a4cff107963036
Eliran Levi <eliran@stratoscale.com>
2018-01-04 14:10:27 +0200

    WIP

--- treelib/src/main.cpp
+++ treelib/src/main.cpp
@@ -2,11 +2,16 @@
 
 int main() {
     treelib::Tree<int> tree;
-    tree.create_node("Dumb people", "dumb", 3);
+    tree.create_node("People", "people", 3);
+    tree.create_node("Dumb people", "dumb", "people", 3);
     tree.create_node("Rich dumb people", "rich-dumb", "dumb", 6);
     tree.create_node("Poor dumb people", "poor-dumb", "dumb", 8);
-    tree.create_node("Smart people", "dumb", 11);
-    tree.create_node("Rich smart people", "rich-smart", "dumb", 12);
-    tree.create_node("Poor smart people", "poor-smart", "dumb", 13);
+    tree.create_node("Smart people", "smart", "people", 11);
+    tree.create_node("Rich smart people", "rich-smart", "smart", 12);
+    tree.create_node("Poor smart people", "poor-smart", "smart", 13);
+
+    auto children = tree.children("dumb");
+    std::cout << children[0]->data << std::endl;
+    std::cout << children[1]->data << std::endl;
     return 0;
 }
--- treelib/src/treelib.h
+++ treelib/src/treelib.h
@@ -3,38 +3,88 @@
 
 #include <vector>
 #include <string>
+#include <map>
+#include <iostream>
+
+namespace treelib {
 
 template <class T>
 class Node
 {
 public:
-    Node(std::string tag, std::string ID, T value);
+    Node(std::string _tag, std::string _identifier, T _data, std::string parent);
 
-    T value;
     std::string tag;
-    std::string ID;
+    std::string identifier;
+    std::string parent;
+    T data;
+    std::vector< Node<T> * > children;
+    void add_child(Node<T> *node);
 
 private:
     Node *parent_node;
-    std::vector<Node*> children;
-}
+};
 
 template <class T>
-class Treelib
+class Tree
 {
 public:
-    Treelib(void);
+    Tree(void) : root(nullptr) {
 
-    Node<T>* create_node(std::string tag, std::string identifier, std::string parent, T value);
+    }
+
+    Node<T>* create_node(std::string tag, std::string identifier, T data) {
+        if (nullptr != root) {
+            std::cout << "A root node already exists.";
+            throw "A root node already exists.";
+        }
+        return create_node(tag, identifier, "", data);
+    }
+
+    Node<T>* create_node(std::string tag, std::string identifier, std::string parent, T data) {
+        if (node_map.count(identifier)) {
+            std::cout << "Duplicate node";
+            throw "Duplicate node";
+        }
+        Node<T> * node = new Node<T>(tag, identifier, data, "");
+        node_map[identifier] = node;
+
+        if (parent.length()) {
+            Node<T> *parent_node = get_node(parent);
+            parent_node->add_child(node);
+        }
+        return node;
+    }
+
+    Node<T>* get_node(std::string identifier) {
+        if (node_map.count(identifier))
+            return node_map[identifier];
+        return nullptr;
+    }
+
+    std::vector< Node<T> * > & children(std::string identifier) {
+        return node_map[identifier]->children;
+    }
 
 private:
-    Node root;
-}
+    Node<T>* root;
 
+    std::map<std::string, Node<T> * > node_map;
+};
 
 template <class T>
-Node<T>* Node<T>::create_node(T value) {
-
+Node<T>::Node(std::string _tag, std::string _identifier, T _data, std::string _parent) {
+    tag = _tag;
+    identifier = _identifier;
+    data = _data;
+    parent = _parent;
 }
+
+template <class T>
+void Node<T>::add_child(Node<T> *child) {
+    children.insert(children.begin(), child);
+}
+
+} // namespace treelib
 
 #endif
